a8562c2aa0f041b1428a4f22932b87889eeb7a71
정태현 <2018009116@hanyang.ac.kr>
2023-03-18 09:03:03 +0000

    practice_1

--- xv6-public/defs.h
+++ xv6-public/defs.h
@@ -188,3 +188,6 @@ void            clearpteu(pde_t *pgdir, char *uva);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+// prac_syscall.c
+int             myfunction(char*);
