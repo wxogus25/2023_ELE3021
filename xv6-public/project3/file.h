8313234f9153a622035cd8cc8f08967c3213fc47
정태현 <2018009116@hanyang.ac.kr>
2023-06-08 09:05:23 +0000

    project3

---
+++ xv6-public/project3/file.h
@@ -0,0 +1,42 @@
+struct file {
+  enum { FD_NONE, FD_PIPE, FD_INODE } type;
+  int ref; // reference count
+  char readable;
+  char writable;
+  struct pipe *pipe;
+  struct inode *ip;
+  uint off;
+};
+
+
+// in-memory copy of an inode
+struct inode {
+  uint dev;           // Device number
+  uint inum;          // Inode number
+  int ref;            // Reference count
+  struct sleeplock lock; // protects everything below here
+  int valid;          // inode has been read from disk?
+
+  short type;         // copy of disk inode
+  short major;
+  short minor;
+  short nlink;
+  uint size;
+  uint addrs[NDIRECT+1]; // 이것도 변경
+  // addrs 완전히 공유하면 두 파일은 같은 파일 공유
+  // 하드링크는 inode 같이 공유하면 안 됨
+  // 심볼릭 링크는 A 파일을 열 때 B를 열게 하면 됨
+  // file type 변경하고 path 추가해서 handling 하기
+  // sys_open 수정하면 될 듯
+};
+
+// table mapping major device number to
+// device functions
+struct devsw {
+  int (*read)(struct inode*, char*, int);
+  int (*write)(struct inode*, char*, int);
+};
+
+extern struct devsw devsw[];
+
+#define CONSOLE 1
